d0ff6c92320c66e5f20e4ec04abc45f0364e5fd0
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2023-05-07 13:36:59 -0500

    Created the controller AccountController to implement the Log in and Log out.

---
+++ WashingCars/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WashingCars.DAL;
+using WashingCars.Helpers;
+using WashingCars.Models;
+
+namespace WashingCars.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserHelper _userHelper;
+        private readonly DatabaseContext _context;
+
+        public AccountController(IUserHelper userHelper, DatabaseContext context)
+        {
+            _userHelper = userHelper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(loginViewModel);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+            }
+            return View(loginViewModel);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Unauthorized()
+        {
+            return View();
+        }
+    }
+}
