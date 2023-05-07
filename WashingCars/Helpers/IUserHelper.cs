acfcd7b6c464f562555945fdcee0a5ad6bc133cf
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2023-05-07 11:29:16 -0500

    New created interface IUserHelper with your implementation methods and created the view LoginViewModel.

---
+++ WashingCars/Helpers/IUserHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WashingCars.DAL.Entities;
+using WashingCars.Models;
+
+namespace WashingCars.Helpers
+{
+    public interface IUserHelper
+    {
+        #region method signatures
+        Task<User> GetUserAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+        Task<SignInResult> LoginAsync(LoginViewModel loginViewModel);
+
+        Task LogoutAsync();
+
+        #endregion
+    }
+}
